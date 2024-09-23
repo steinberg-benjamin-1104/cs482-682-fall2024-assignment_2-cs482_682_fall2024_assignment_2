d0080a4951d6b5e4d752460b171010d05a97a8cd
steinberg-benjamin-1104 <98624777+steinberg-benjamin-1104@users.noreply.github.com>
2024-09-22 21:27:40 -0700

    Add files via upload

--- kmeans_clustering.py
+++ kmeans_clustering.py
@@ -2,30 +2,48 @@ import numpy as np
 import argparse
 import scipy.io
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
 class MykmeansClustering:
     def __init__(self, dataset_file):
         self.model = None
-        
         self.dataset_file = dataset_file
+        self.data = None
         self.read_mat()
 
     def read_mat(self):
         mat = scipy.io.loadmat(self.dataset_file)
-        
-    def model_fit(self):
+        self.data = mat['X']
+        print(f"Data loaded. Shape of 'X': {self.data.shape}")
+
+    def model_fit(self, n_clusters=3):
         '''
-        initialize self.model here and execute kmeans clustering here
+        Initialize self.model and execute KMeans clustering on self.data
         '''
-        
-        cluster_centers = np.array([[0,0]])
-        return cluster_centers
+        self.model = KMeans(n_clusters=n_clusters, random_state=0)
+        self.model.fit(self.data)
+        return self.model.cluster_centers_, self.model.labels_
+
+    def plot_clusters(self, cluster_centers, labels):
+        '''
+        Plot the data points and cluster centers
+        '''
+        plt.figure(figsize=(8, 6))
+        plt.scatter(self.data[:, 0], self.data[:, 1], c=labels, cmap='viridis', s=50, alpha=0.6)
+        plt.scatter(cluster_centers[:, 0], cluster_centers[:, 1], c='red', marker='x', s=200, label='Centroids')
+        plt.title('KMeans Clustering')
+        plt.xlabel('Feature 1')
+        plt.ylabel('Feature 2')
+        plt.legend()
+        plt.show()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Kmeans clustering')
-    parser.add_argument('-d','--dataset_file', type=str, default = "dataset_q2.mat", help='path to dataset file')
+    parser.add_argument('-d','--dataset_file', type=str, default="dataset_q2.mat", help='path to dataset file')
     args = parser.parse_args()
     classifier = MykmeansClustering(args.dataset_file)
-    clusters_centers = classifier.model_fit()
-    print(clusters_centers)
+    cluster_centers, labels = classifier.model_fit(n_clusters=7)
+    print("Cluster Centers:")
+    print(cluster_centers)
     
+    classifier.plot_clusters(cluster_centers, labels)
--- logistic_regression.py
+++ logistic_regression.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression,LinearRegression
+from sklearn.linear_model import LogisticRegression, LinearRegression
 import argparse
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+import matplotlib.pyplot as plt
 
 class MyLogisticRegression:
     def __init__(self, dataset_num, perform_test):
@@ -27,65 +28,99 @@ class MyLogisticRegression:
             train_dataset_file = 'train_q1_2.csv'
             test_dataset_file = 'test_q1_2.csv'
         else:
-            print("unsupported dataset number")
+            raise ValueError("Unsupported dataset number")
             
         self.training_set = pd.read_csv(train_dataset_file, sep=',', header=0)
+        self.X_train = self.training_set[['exam_score_1', 'exam_score_2']]
+        self.y_train = self.training_set['label']
+        
         if self.perform_test:
             self.test_set = pd.read_csv(test_dataset_file, sep=',', header=0)
-        
+            self.X_test = self.test_set[['exam_score_1', 'exam_score_2']]
+            self.y_test = self.test_set['label']
         
     def model_fit_linear(self):
-        '''
-        initialize self.model_linear here and call the fit function
-        '''
-        pass
+        '''Initialize and fit the linear regression model.'''
+        self.model_linear = LinearRegression()
+        self.model_linear.fit(self.X_train, self.y_train)
     
     def model_fit_logistic(self):
-        '''
-        initialize self.model_logistic here and call the fit function
-        '''
-
-        pass
+        '''Initialize and fit the logistic regression model.'''
+        self.model_logistic = LogisticRegression()
+        self.model_logistic.fit(self.X_train, self.y_train)
     
     def model_predict_linear(self):
-        '''
-        Calculate and return the accuracy, precision, recall, f1, support of the model.
-        '''
+        '''Predict using the linear model and return accuracy, precision, recall, f1, and support.'''
         self.model_fit_linear()
-        accuracy = 0.0
-        precision, recall, f1, support = np.array([0,0]), np.array([0,0]), np.array([0,0]), np.array([0,0])
-        assert self.model_linear is not None, "Initialize the model, i.e. instantiate the variable self.model_linear in model_fit_linear method"
-        assert self.training_set is not None, "self.read_csv function isn't called or the self.trianing_set hasn't been initialized "
-        
-        if self.X_test is not None:
-            # perform prediction here
-            pass
-        
-        assert precision.shape == recall.shape == f1.shape == support.shape == (2,), "precision, recall, f1, support should be an array of shape (2,)"
-        return [accuracy, precision, recall, f1, support]
+        assert self.model_linear is not None, "Model not initialized. Call model_fit_linear first."
+        assert self.training_set is not None, "Training data not initialized."
+
+        if self.perform_test and self.X_test is not None:
+            predictions = np.round(self.model_linear.predict(self.X_test))
+            accuracy = accuracy_score(self.y_test, predictions)
+            precision, recall, f1, support = precision_recall_fscore_support(self.y_test, predictions, average=None)
+            assert precision.shape == recall.shape == f1.shape == support.shape == (2,), "precision, recall, f1, support should be an array of shape (2,)"
+            return [accuracy, precision, recall, f1, support]
 
     def model_predict_logistic(self):
-        '''
-        Calculate and return the accuracy, precision, recall, f1, support of the model.
-        '''
+        '''Predict using the logistic model and return accuracy, precision, recall, f1, and support.'''
         self.model_fit_logistic()
-        accuracy = 0.0
-        precision, recall, f1, support = np.array([0,0]), np.array([0,0]), np.array([0,0]), np.array([0,0])
-        assert self.model_logistic is not None, "Initialize the model, i.e. instantiate the variable self.model_logistic in model_fit_logistic method"
-        assert self.training_set is not None, "self.read_csv function isn't called or the self.trianing_set hasn't been initialized "
-        if self.X_test is not None:
-            # perform prediction here
-            pass
-        
-        assert precision.shape == recall.shape == f1.shape == support.shape == (2,), "precision, recall, f1, support should be an array of shape (2,)"
-        return [accuracy, precision, recall, f1, support]
+        assert self.model_logistic is not None, "Model not initialized. Call model_fit_logistic first."
+        assert self.training_set is not None, "Training data not initialized."
 
+        if self.perform_test and self.X_test is not None:
+            predictions = self.model_logistic.predict(self.X_test)
+            accuracy = accuracy_score(self.y_test, predictions)
+            precision, recall, f1, support = precision_recall_fscore_support(self.y_test, predictions, average=None)
+            assert precision.shape == recall.shape == f1.shape == support.shape == (2,), "precision, recall, f1, support should be an array of shape (2,)"
+            return [accuracy, precision, recall, f1, support]
+
+    def matplot(self, model_type="logistic"):
+        '''Visualize the decision boundary for logistic or linear model.'''
+        if model_type == "logistic":
+            self.model_fit_logistic()
+            plot_decision_boundary(self.X_train, self.y_train, self.model_logistic)
+        elif model_type == "linear":
+            self.model_fit_linear()
+            plot_decision_boundary(self.X_train, self.y_train, self.model_linear)
+
+# Plot Decision Boundary Function
+def plot_decision_boundary(X, y, model):
+    '''Plot the decision boundary for any model.'''
+    X = X.values if isinstance(X, pd.DataFrame) else X
+    
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01), np.arange(y_min, y_max, 0.01))
+    
+    Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
+    
+    if isinstance(model, LinearRegression):
+        Z = np.round(Z)
+    
+    Z = Z.reshape(xx.shape)
+    
+    plt.contourf(xx, yy, Z, alpha=0.8)
+    plt.scatter(X[:, 0], X[:, 1], c=y, edgecolors='k', marker='o', s=100)
+    plt.title('Decision Boundary')
+    plt.xlabel('Exam Score 1')
+    plt.ylabel('Exam Score 2')
+    plt.show()
+
+# Main function with argparse
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Linear Regression')
-    parser.add_argument('-d','--dataset_num', type=str, default = "1", choices=["1","2"], help='string indicating datset number. For example, 1 or 2')
-    parser.add_argument('-t','--perform_test', action='store_true', help='boolean to indicate inference')
+    parser = argparse.ArgumentParser(description='Linear/Logistic Regression')
+    parser.add_argument('-d', '--dataset_num', type=str, default="1", choices=["1", "2"], help='Dataset number: 1 or 2')
+    parser.add_argument('-t', '--perform_test', action='store_true', help='Boolean flag for testing mode')
     args = parser.parse_args()
     classifier = MyLogisticRegression(args.dataset_num, args.perform_test)
-    acc = classifier.model_predict_linear()
-    acc = classifier.model_predict_logistic()
-    
+
+    linear_results = classifier.model_predict_linear()
+    print("Linear Regression Results:", linear_results)
+    logistic_results = classifier.model_predict_logistic()
+    print("Logistic Regression Results:", logistic_results)
+
+    # matplot
+    print("\nVisualizing decision boundaries...\n")
+    classifier.matplot(model_type="logistic")
+    classifier.matplot(model_type="linear")
